9372a9653190e557d6c3cd5b0cc91ef4572dc442
Alex. M <alllexmi@gmail.com>
2020-12-12 21:46:09 +0300

    Final1

--- 1.2.py
+++ 1.2.py
@@ -2,8 +2,5 @@
 # выведите в формате чч:мм:сс. Используйте форматирование строк.
 
 user_input = int(input('Введите время в секундах: '))
-hours = user_input // 3600
-minutes = user_input % 3600 // 60
-seconds = user_input % 60
-# result = int(f"{hours:02}':'{minutes:02}':'{seconds:02}")
-print(f"{hours:02}:{minutes:02}:{seconds:02}")
+
+print(f"{user_input // 3600 :02}:{user_input % 3600 // 60 :02}:{user_input % 60 :02}")
