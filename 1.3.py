c5dcac384cbde21134748628c6930b9d87a16b90
Alex. M <alllexmi@gmail.com>
2020-12-13 12:25:44 +0300

    success!

--- 1.3.py
+++ 1.3.py
@@ -1,11 +1,10 @@
 n = str(input('Введите число: '))
 
-a = n + n
-b = n + n + n
-r = int(n) + int(a) + int(b)
-print(a)
-print(b)
-print(r)
+# a = n + n
+# b = n + n + n
+# r = int(n) + int(a) + int(b)
+# print(a)
+# print(b)
+# print(r)
 
-# I don`t know, but it works!
-print(f"{int(n) + int(n + n) + int(n + n + n)}")
+print(f"{int(n) + int(n + n) + int(n + n + n)}") # I don`t know how, but it works too!
