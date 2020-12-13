89b1a6bfc8bb220305bf3212b563ebdb85c9045a
Alex. M <alllexmi@gmail.com>
2020-12-13 20:22:42 +0300

    1.5

---
+++ 1.5.py
@@ -0,0 +1,12 @@
+a = int(input('Введите прибыль: '))
+b = int(input('Введите издержки: '))
+c = int(input('Введите количество сотрудников: '))
+income = a - b
+coeff = a / b
+person_income = c / income
+if a > b:
+    print('У Вас прибыль: ', income)
+else:
+    print('У Вас убыток: ', income)
+print('Соотношение прибыли в издержкам: ', coeff)
+print('Прибыль на 1 сотрудника: ', person_income)
