35e02c174c0e8caf513410094b3118d90e5d9caf
Alex. M <alllexmi@gmail.com>
2020-12-18 12:58:29 +0300

    2.

--- 2.5.py
+++ 2.5.py
@@ -16,14 +16,13 @@ print(f"Рейтинг - {my_list}")
 digit = int(input("(Для выхода - 111) \nВведите число:  "))
 while digit != 111:
     for el in range(len(my_list)):
-        if my_list[el] == digit:
-            my_list.insert(el + 1, digit)
-            break
-        elif my_list[0] < digit:
+        if my_list[0] < digit:
             my_list.insert(0, digit)
+            break
         elif my_list[-1] > digit:
             my_list.append(digit)
-        elif my_list[el] > digit > my_list[el + 1]:
+            break
+        elif my_list[el] >= digit >= my_list[el + 1]:
             my_list.insert(el + 1, digit)
             break
     print(f"Новый рейтинг - {my_list}")
